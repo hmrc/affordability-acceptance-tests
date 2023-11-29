72dd7b65c7a391644087f72ed9dcfa66691e646c
JamieNorth <75728919+JamieNorth@users.noreply.github.com>
2023-11-29 11:47:48 +0000

    Update DriverActions.scala

--- src/test/scala/uk/gov/hmrc/test/ui/stepdefs/other/DriverActions.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/stepdefs/other/DriverActions.scala
@@ -31,7 +31,6 @@ trait DriverActions extends WebBrowser {
 
   implicit def driver: WebDriver = {
     val options = new ChromeOptions
-    options.addArguments("--remote-allow-origins=*")
     val runZap = sys.props.getOrElse("zapBrowser", "false").toBoolean
     if (runZap) SingletonDriver.getInstance(Some(options))
     else SingletonDriver.getInstance(Some(options))
