a5d33a9972899656610ba9eb77f5b2d4e268cf62
JamieNorth <75728919+JamieNorth@users.noreply.github.com>
2025-05-09 15:49:42 +0100

    Update Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     "com.vladsch.flexmark" % "flexmark-all" % "0.64.8" % Test,
     "org.scalatest" %% "scalatest" % "3.2.19" % Test,
     "org.scalatestplus" %% "selenium-4-2" % "3.2.13.0" % Test,
-    "io.cucumber" %% "cucumber-scala" % "8.27.0" % Test,
+    "io.cucumber" %% "cucumber-scala" % "8.27.1" % Test,
     "io.cucumber" % "cucumber-junit" % "7.22.1" % Test,
     "junit" % "junit" % "4.13.2" % Test,
     "uk.gov.hmrc" %% "ui-test-runner" % "0.45.0" % Test,
