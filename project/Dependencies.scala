91fe8d3fc63ddbc04190e03324f4a5fc89bdac69
JamieNorth <75728919+JamieNorth@users.noreply.github.com>
2025-07-09 13:05:40 +0100

    Update Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,12 +8,12 @@ object Dependencies {
     "org.scalatest" %% "scalatest" % "3.2.19" % Test,
     "org.scalatestplus" %% "selenium-4-2" % "3.2.13.0" % Test,
     "io.cucumber" %% "cucumber-scala" % "8.28.0" % Test,
-    "io.cucumber" % "cucumber-junit" % "7.23.0" % Test,
+    "io.cucumber" % "cucumber-junit" % "7.24.0" % Test,
     "junit" % "junit" % "4.13.2" % Test,
-    "uk.gov.hmrc" %% "ui-test-runner" % "0.46.0" % Test,
+    "uk.gov.hmrc" %% "ui-test-runner" % "0.47.0" % Test,
     "com.novocode" % "junit-interface" % "0.11" % Test,
-    "io.cucumber" % "cucumber-picocontainer" % "7.23.0" % Test,
-    "org.playframework" %% "play-json" % "3.0.4" % Test,
+    "io.cucumber" % "cucumber-picocontainer" % "7.24.0" % Test,
+    "org.playframework" %% "play-json" % "3.0.5" % Test,
     ("org.mongodb.scala" %% "mongo-scala-driver" % "5.5.1").cross(CrossVersion.for3Use2_13)
   )
 
