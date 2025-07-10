685f0e5ce9f9b97dcbbfe4382d8d0e1fddfc1d1c
JamieNorth <75728919+JamieNorth@users.noreply.github.com>
2025-07-10 09:48:41 +0100

    Update Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,14 +3,14 @@ import sbt._
 object Dependencies {
 
   val test = Seq(
-    "com.typesafe" % "config" % "1.4.3" % Test,
+    "com.typesafe" % "config" % "1.4.4" % Test,
     "com.vladsch.flexmark" % "flexmark-all" % "0.64.8" % Test,
     "org.scalatest" %% "scalatest" % "3.2.19" % Test,
     "org.scalatestplus" %% "selenium-4-2" % "3.2.13.0" % Test,
     "io.cucumber" %% "cucumber-scala" % "8.28.0" % Test,
     "io.cucumber" % "cucumber-junit" % "7.24.0" % Test,
     "junit" % "junit" % "4.13.2" % Test,
-    "uk.gov.hmrc" %% "ui-test-runner" % "0.47.0" % Test,
+    "uk.gov.hmrc" %% "ui-test-runner" % "0.46.0" % Test,
     "com.novocode" % "junit-interface" % "0.11" % Test,
     "io.cucumber" % "cucumber-picocontainer" % "7.24.0" % Test,
     "org.playframework" %% "play-json" % "3.0.5" % Test,
